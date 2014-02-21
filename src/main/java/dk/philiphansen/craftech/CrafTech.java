9bb807874823f6dc7fb967577cf8a21ef2024d09
Philip Hansen <me@philiphansen.dk>
2014-02-21 16:22:29 +0100

    Use proper logger
    Switched to use logger rather than just a print statement, and did a
    little other code clean up.

--- src/main/java/dk/philiphansen/craftech/CrafTech.java
+++ src/main/java/dk/philiphansen/craftech/CrafTech.java
@@ -1,19 +1,28 @@
 package dk.philiphansen.craftech;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = CrafTech.MODID, version = CrafTech.VERSION)
-public class CrafTech
-{
+@Mod(modid = CrafTech.MODID, name = CrafTech.NAME, version = CrafTech.VERSION)
+public class CrafTech {
     public static final String MODID = "craftech";
-    public static final String VERSION = "1.0";
+    public static final String NAME = "CrafTech";
+    public static final String VERSION = "${version}";
+    
+    public static final Logger logger = LogManager.getLogger(CrafTech.NAME);
+    
+    @Instance(CrafTech.NAME)
+    public static CrafTech instance;
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		System.out.println("Welcome to the new age!");
+		logger.info("Welcome to the new age!");
     }
 }
