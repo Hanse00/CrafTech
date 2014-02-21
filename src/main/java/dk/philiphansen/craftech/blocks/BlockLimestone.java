b6f66d5205518e533d00641fc58d7b40b06a166a
Philip Hansen <me@philiphansen.dk>
2014-02-21 18:18:17 +0100

    Add proper block functions
    Block can now be mined, has same hardness and sound as stone, also
    includes limestone texture.

--- src/main/java/dk/philiphansen/craftech/blocks/BlockLimestone.java
+++ src/main/java/dk/philiphansen/craftech/blocks/BlockLimestone.java
@@ -4,15 +4,19 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockLimestone extends Block {
 
 	protected BlockLimestone() {
-		super(Material.iron);
-		setBlockName("blockLimestone");
+		super(Material.rock);
+		setBlockName(ModBlocks.LIMESTONE_NAME);
 		setCreativeTab(CreativeTabs.tabBlock);
+		setHardness(1.5F);
+		setResistance(10.0F);
+		setStepSound(soundTypePiston);
 	}
 	
 	@SideOnly(Side.CLIENT)
--- src/main/java/dk/philiphansen/craftech/blocks/ModBlocks.java
+++ src/main/java/dk/philiphansen/craftech/blocks/ModBlocks.java
@@ -4,11 +4,13 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModBlocks {
 	
+	public static final String LIMESTONE_NAME = "blockLimestone";
+	
 	public static BlockLimestone BlockLimestone;
 
 	public static void init() {
 		BlockLimestone = new BlockLimestone();
 		
-		GameRegistry.registerBlock(BlockLimestone, "BlockLimestone");
+		GameRegistry.registerBlock(BlockLimestone, ModBlocks.LIMESTONE_NAME);
 	}
 }
