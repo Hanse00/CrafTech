ad20585a44a0064a054e59a3da6d5ba683720584
Philip Hansen <me@philiphansen.dk>
2014-02-21 17:48:49 +0100

    Use custom texture

--- src/main/java/dk/philiphansen/craftech/blocks/BlockLimestone.java
+++ src/main/java/dk/philiphansen/craftech/blocks/BlockLimestone.java
@@ -2,15 +2,23 @@ package dk.philiphansen.craftech.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockLimestone extends Block {
 
 	protected BlockLimestone() {
 		super(Material.iron);
-		setBlockName("BlockLimestone");
-		setCreativeTab(CreativeTabs.tabBlock);
 		setBlockName("blockLimestone");
+		setCreativeTab(CreativeTabs.tabBlock);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerBlockIcons(IIconRegister register) {
+		blockIcon = register.registerIcon("craftech:limestone");
 	}
 	
 }
