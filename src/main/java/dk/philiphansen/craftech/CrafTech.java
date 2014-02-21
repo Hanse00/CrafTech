0d6ac1aabcde7403d13d92d176899e35ba1371e7
Philip Hansen <me@philiphansen.dk>
2014-02-21 14:27:06 +0100

    Edited examplemod to craftech

--- src/main/java/dk/philiphansen/craftech/CrafTech.java
+++ src/main/java/dk/philiphansen/craftech/CrafTech.java
@@ -1,20 +1,19 @@
-package com.example.examplemod;
+package dk.philiphansen.craftech;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = CrafTech.MODID, version = CrafTech.VERSION)
+public class CrafTech
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "craftech";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+		System.out.println("Welcome to the new age!");
     }
 }
