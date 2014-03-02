05cbcc5a0dcbf3b832726ae6679b00b71360c2f7
Philip Hansen <me@philiphansen.dk>
2014-03-03 00:38:35 +0100

    Bumped version number

--- src/main/java/dk/philiphansen/craftech/reference/ModInfo.java
+++ src/main/java/dk/philiphansen/craftech/reference/ModInfo.java
@@ -20,5 +20,5 @@ package dk.philiphansen.craftech.reference;
 public class ModInfo {
 	public static final String MODID = "craftech";
     public static final String NAME = "CrafTech";
-    public static final String VERSION = "0.1a1";
+    public static final String VERSION = "0.1a2";
 }
