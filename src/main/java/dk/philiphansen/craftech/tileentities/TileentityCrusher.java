63282fd96d81825b18354a38ef4c2c9aef8fd854
yonsaber <matt@mattredmond.co.uk>
2014-03-02 22:43:53 +0000

    Added sided inventory to crusher

--- src/main/java/dk/philiphansen/craftech/tileentities/TileentityCrusher.java
+++ src/main/java/dk/philiphansen/craftech/tileentities/TileentityCrusher.java
@@ -19,16 +19,19 @@ package dk.philiphansen.craftech.tileentities;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
+
+import com.google.common.primitives.Ints;
+
 import dk.philiphansen.craftech.blocks.ModBlocks;
 import dk.philiphansen.craftech.items.ModItems;
 
-public class TileentityCrusher extends TileEntity implements IInventory {
+public class TileentityCrusher extends TileEntity implements ISidedInventory {
 
 	private ItemStack[] items;
 	private int processTimer;
@@ -296,5 +299,32 @@ public class TileentityCrusher extends TileEntity implements IInventory {
 	//Set the length of the process timer
 	public void setTimer(int processTimer) {
 		this.processTimer = processTimer;
+	}
+
+	@Override
+	public int[] getAccessibleSlotsFromSide(int i) {
+		if (i == 0) {
+			return new int[]{1};
+		} else {
+			return new int[]{0};
+		}
+	}
+
+	@Override
+	public boolean canInsertItem(int slot, ItemStack stack, int side) {
+		if (Ints.contains(getAccessibleSlotsFromSide(side), slot) && isItemValidForSlot(slot, stack)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean canExtractItem(int slot, ItemStack stack, int side) {
+		if (Ints.contains(getAccessibleSlotsFromSide(side), slot) && getStackInSlot(slot) == stack) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
