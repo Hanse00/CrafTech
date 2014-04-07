0584e4b24812472175eb1d56cbf71e8061581c3c
Philip Hansen <me@philiphansen.dk>
2014-04-07 09:41:13 +0200

    Switched to JUnit 4 assert

--- src/test/java/dk/philiphansen/craftech/CrafTechTest.java
+++ src/test/java/dk/philiphansen/craftech/CrafTechTest.java
@@ -20,7 +20,7 @@
 package dk.philiphansen.craftech;
 
 import cpw.mods.fml.common.Mod;
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
