442090876c8bbbaf76b7710c78c92dea2744eb1d
Philip Hansen <me@philiphansen.dk>
2014-04-08 10:04:03 +0200

    Clean slate, shall we?

--- src/main/java/dk/philiphansen/craftech/CrafTech.java
+++ src/main/java/dk/philiphansen/craftech/CrafTech.java
@@ -19,91 +19,34 @@
 
 package dk.philiphansen.craftech;
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import dk.philiphansen.craftech.block.ModBlocks;
-import dk.philiphansen.craftech.creativetab.CreativeTabCrafTech;
-import dk.philiphansen.craftech.event.CraftEvent;
-import dk.philiphansen.craftech.handler.*;
-import dk.philiphansen.craftech.item.ModItems;
-import dk.philiphansen.craftech.reference.ModInfo;
-import net.minecraft.creativetab.CreativeTabs;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * Mod base class, this is where everything is called from.
- * Forge Mod Loader calls event handlers in this class when Minecraft is loading.
- * Those calls in these three methods are responsible for setting up the mod as the game loads.
- */
-@Mod(modid = ModInfo.ID, name = ModInfo.NAME, version = ModInfo.VERSION)
+@Mod(modid = "craftech", name = "CrafTech", version = "${version}")
 public class CrafTech {
-
-	public static final CreativeTabs tabCrafTech = new CreativeTabCrafTech();
-	public static final Logger logger = LogManager.getLogger(ModInfo.NAME);
-
-	@Instance(ModInfo.ID)
+	public static final Logger logger = LogManager.getLogger("CrafTech");
+	@Instance("craftech")
 	public static CrafTech instance;
 
-	/**
-	 * Forge Mod loader pre-initialization.
-	 * This is called before FML really "loads" the mod,
-	 * useful for loading things the rest of the load sequence depends on.
-	 * (eg. Load blocks and items before recipes are loaded)
-	 *
-	 * @param event FML pre-init event, can be used to get info about the file system.
-	 */
 	@EventHandler
+	@SuppressWarnings("unused")
 	public void preInit(FMLPreInitializationEvent event) {
-		ConfigHandler.init(event.getSuggestedConfigurationFile());
-
-		ModBlocks.init();
-		ModItems.init();
 	}
 
-	/**
-	 * Forge Mod loader initialization.
-	 * This is where forge loads the mod, and all it's components.
-	 *
-	 * @param event Event passed by FML, this can be used to get the state of the mod.
-	 */
 	@EventHandler
+	@SuppressWarnings("unused")
 	public void init(FMLInitializationEvent event) {
-		ModBlocks.initTileEntities();
-
-		ModBlocks.initCrafting();
-		ModBlocks.initSmelting();
-		ModBlocks.initTechTableRecipes();
-
-		ModItems.initCrafting();
-		ModItems.initSmelting();
-		ModItems.initCrusher();
-
-		ModItems.genRecipes();
-
-		new FuelHandler();
-		new GenerationHandler();
-		new GuiHandler();
-		new AchievementHandler();
-
-		FMLCommonHandler.instance().bus().register(new CraftEvent());
 	}
 
-	/**
-	 * Forge Mod Loader post initialization.
-	 * This is called after FML has loaded all the mods,
-	 * likely where you would put any inter-mod code.
-	 *
-	 * @param event FML event, can be used to build dependencies on other mods.
-	 */
 	@EventHandler
+	@SuppressWarnings("unused")
 	public void postInit(FMLPostInitializationEvent event) {
-	    /* A debug message to check that the mod has loaded */
 		logger.info("Welcome to the new age!");
 	}
 }
